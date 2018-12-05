bb5f952a37178e798f62ca499ea2190ae1335eb5
Jonathan.e <jonathanerquinigo@pursuit.org>
2018-12-04 21:12:47 -0500

    secondUpdate

--- express.js
+++ express.js
@@ -1,1 +1,33 @@
-//first commit
+const express = require("express");
+const app = express();
+
+app.get("/math/:add/", (req, res) => {
+  // console.log(req.params.val);
+
+  let num1 = req.query.num1;
+  let num2 = req.query.num2;
+  let operator = req.params.add
+  console.log(operator,"this");
+
+  if (!isNaN(num1) && !isNaN(num2)) {
+
+  } if(operator === "+" || operator === "add"){
+      let sum = parseInt(num1) + parseInt(num2);
+        res.json({ input: req.query, data: sum });
+  } if(operator === "*" || operator === "mult"){
+      let mult = parseInt(num1) * parseInt(num2);
+        res.json({input: req.query, data: mult })
+  } if(operator === "/" || operator === "div"){
+      let div = parseInt(num1) / parseInt(num2)
+        res.json({input: req.query, data: div})
+  } if(operator === "-" || operator === "sub"){
+      let sub = parseInt(num1) - parseInt(num2)
+        res.json({input: req.query, data: sub})
+  }else {
+    res.send("please enter valid numbers and route");
+  }
+});
+
+app.listen(8000, () => {
+  console.log("this is port 8000");
+});
